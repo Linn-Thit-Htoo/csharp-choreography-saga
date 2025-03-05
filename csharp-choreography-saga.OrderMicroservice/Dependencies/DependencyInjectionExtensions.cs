c5a33eba1ea54b8f2027657218ff88432e697869
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-05 06:44:11 +0630

    -

--- csharp-choreography-saga.OrderMicroservice/Dependencies/DependencyInjectionExtensions.cs
+++ csharp-choreography-saga.OrderMicroservice/Dependencies/DependencyInjectionExtensions.cs
@@ -7,59 +7,58 @@ using csharp_choreography_saga.OrderMicroservice.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace csharp_choreography_saga.OrderMicroservice.Dependencies
+namespace csharp_choreography_saga.OrderMicroservice.Dependencies;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        builder.Services.Configure<AppSetting>(builder.Configuration);
+
+        builder
+            .Services.AddControllers()
+            .ConfigureApiBehaviorOptions(opt =>
+            {
+                opt.InvalidModelStateResponseFactory = context =>
+                {
+                    var errors = context
+                        .ModelState.Where(ms => ms.Value!.Errors.Any())
+                        .SelectMany(ms => ms.Value!.Errors.Select(e => e.ErrorMessage))
+                        .ToList();
+
+                    var errorMessage = string.Join("; ", errors);
+                    var result = Result<object>.Fail(
+                        errorMessage
+                    );
+
+                    return new OkObjectResult(result);
+                };
+            })
+            .AddJsonOptions(opt =>
+            {
+                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+
+        builder.Services.AddDbContext<AppDbContext>((serviceProvider, opt) =>
         {
-            builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
+            opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection"));
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        });
 
-            builder.Services.Configure<AppSetting>(builder.Configuration);
+        builder.Services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
+        });
 
-            builder
-                .Services.AddControllers()
-                .ConfigureApiBehaviorOptions(opt =>
-                {
-                    opt.InvalidModelStateResponseFactory = context =>
-                    {
-                        var errors = context
-                            .ModelState.Where(ms => ms.Value!.Errors.Any())
-                            .SelectMany(ms => ms.Value!.Errors.Select(e => e.ErrorMessage))
-                            .ToList();
+        builder.Services.AddTransient(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+        builder.Services.AddHostedService<RabbitMQService>();
+        builder.Services.AddSingleton<IBus, RabbitBus>();
+        builder.Services.AddScoped<IOrderService, OrderService>();
 
-                        var errorMessage = string.Join("; ", errors);
-                        var result = Result<object>.Fail(
-                            errorMessage
-                        );
-
-                        return new OkObjectResult(result);
-                    };
-                })
-                .AddJsonOptions(opt =>
-                {
-                    opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-                });
-
-            builder.Services.AddDbContext<AppDbContext>((serviceProvider, opt) =>
-            {
-                opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection"));
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            });
-
-            builder.Services.AddMediatR(config =>
-            {
-                config.RegisterServicesFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
-            });
-
-            builder.Services.AddTransient(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
-            builder.Services.AddHostedService<RabbitMQService>();
-            builder.Services.AddSingleton<IBus, RabbitBus>();
-            builder.Services.AddScoped<IOrderService, OrderService>();
-
-            return services;
-        }
+        return services;
     }
 }
