f31e8eea1542eee7d0324dbfc2412082f934578c
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-05 06:45:11 +0630

    -

--- csharp-choreography-saga.OrderMicroservice/Features/CreateOrder/CreateOrderCommandHandler.cs
+++ csharp-choreography-saga.OrderMicroservice/Features/CreateOrder/CreateOrderCommandHandler.cs
@@ -7,78 +7,77 @@ using csharp_choreography_saga.OrderMicroservice.Utils;
 using MediatR;
 using System.Transactions;
 
-namespace csharp_choreography_saga.OrderMicroservice.Features.CreateOrder
+namespace csharp_choreography_saga.OrderMicroservice.Features.CreateOrder;
+
+public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Result<CreateOrderResponse>>
 {
-    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Result<CreateOrderResponse>>
+    private readonly IRepositoryBase<TblOrder> _orderRepository;
+    private readonly IRepositoryBase<TblOrderDetail> _orderDetailRepository;
+    private readonly IBus _bus;
+
+    public CreateOrderCommandHandler(IRepositoryBase<TblOrder> orderRepository, IRepositoryBase<TblOrderDetail> orderDetailRepository, IBus bus)
     {
-        private readonly IRepositoryBase<TblOrder> _orderRepository;
-        private readonly IRepositoryBase<TblOrderDetail> _orderDetailRepository;
-        private readonly IBus _bus;
+        _orderRepository = orderRepository;
+        _orderDetailRepository = orderDetailRepository;
+        _bus = bus;
+    }
 
-        public CreateOrderCommandHandler(IRepositoryBase<TblOrder> orderRepository, IRepositoryBase<TblOrderDetail> orderDetailRepository, IBus bus)
+    public async Task<Result<CreateOrderResponse>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        Result<CreateOrderResponse> result;
+        var orderDetailLst = new List<TblOrderDetail>();
+        using var scope = new TransactionScope(
+            TransactionScopeOption.Required,
+            new TransactionOptions { IsolationLevel = IsolationLevel.RepeatableRead },
+            TransactionScopeAsyncFlowOption.Enabled
+        );
+
+        var order = new TblOrder()
         {
-            _orderRepository = orderRepository;
-            _orderDetailRepository = orderDetailRepository;
-            _bus = bus;
-        }
+            UserId = request.UserId,
+            InvoiceNo = Guid.NewGuid(),
+            GrandTotal = request.GrandTotal,
+            Status = nameof(OrderStatus.Pending)
+        };
+        await _orderRepository.AddAsync(order, cancellationToken);
+        await _orderRepository.SaveChangesAsync(cancellationToken);
 
-        public async Task<Result<CreateOrderResponse>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        if (request.OrderDetails is not null && request.OrderDetails.Count > 0)
         {
-            Result<CreateOrderResponse> result;
-            var orderDetailLst = new List<TblOrderDetail>();
-            using var scope = new TransactionScope(
-                TransactionScopeOption.Required,
-                new TransactionOptions { IsolationLevel = IsolationLevel.RepeatableRead },
-                TransactionScopeAsyncFlowOption.Enabled
-            );
-
-            var order = new TblOrder()
+            foreach (var item in request.OrderDetails)
             {
-                UserId = request.UserId,
-                InvoiceNo = Guid.NewGuid(),
-                GrandTotal = request.GrandTotal,
-                Status = nameof(OrderStatus.Pending)
-            };
-            await _orderRepository.AddAsync(order, cancellationToken);
-            await _orderRepository.SaveChangesAsync(cancellationToken);
-
-            if (request.OrderDetails is not null && request.OrderDetails.Count > 0)
-            {
-                foreach (var item in request.OrderDetails)
+                orderDetailLst.Add(new TblOrderDetail()
                 {
-                    orderDetailLst.Add(new TblOrderDetail()
-                    {
-                        OrderId = order.OrderId,
-                        ProductId = item.ProductId,
-                        InvoiceNo = order.InvoiceNo,
-                        SubTotal = item.SubTotal,
-                        TotalItems = item.TotalItems
-                    });
-                }
-            }
-            await _orderDetailRepository.AddAsync(orderDetailLst, cancellationToken);
-            await _orderDetailRepository.SaveChangesAsync(cancellationToken);
-
-            scope.Complete();
-
-            OrderCreatedEvent orderCreatedEvent = new()
-            {
-                OrderId = order.OrderId,
-                OrderDetails = request.OrderDetails!.Select(x => new OrderDetail()
-                {
-                    ProductId = x.ProductId,
+                    OrderId = order.OrderId,
+                    ProductId = item.ProductId,
                     InvoiceNo = order.InvoiceNo,
-                    SubTotal = x.SubTotal,
-                    TotalItems = x.TotalItems
-                }).ToList()
-            };
-
-            _bus.Send("DirectExchange", "StockQueue", "orderdirect", orderCreatedEvent);
-
-            var model = new CreateOrderResponse() { OrderId = order.OrderId };
-            result = Result<CreateOrderResponse>.Success(model);
-
-            return result;
+                    SubTotal = item.SubTotal,
+                    TotalItems = item.TotalItems
+                });
+            }
         }
+        await _orderDetailRepository.AddAsync(orderDetailLst, cancellationToken);
+        await _orderDetailRepository.SaveChangesAsync(cancellationToken);
+
+        scope.Complete();
+
+        OrderCreatedEvent orderCreatedEvent = new()
+        {
+            OrderId = order.OrderId,
+            OrderDetails = request.OrderDetails!.Select(x => new OrderDetail()
+            {
+                ProductId = x.ProductId,
+                InvoiceNo = order.InvoiceNo,
+                SubTotal = x.SubTotal,
+                TotalItems = x.TotalItems
+            }).ToList()
+        };
+
+        _bus.Send("DirectExchange", "StockQueue", "orderdirect", orderCreatedEvent);
+
+        var model = new CreateOrderResponse() { OrderId = order.OrderId };
+        result = Result<CreateOrderResponse>.Success(model);
+
+        return result;
     }
 }
