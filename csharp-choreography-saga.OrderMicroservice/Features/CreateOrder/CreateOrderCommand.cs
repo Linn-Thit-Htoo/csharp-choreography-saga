eabc066aab5758bfb3ef2c14a489978ebcdb2af3
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-05 06:44:56 +0630

    -

--- csharp-choreography-saga.OrderMicroservice/Features/CreateOrder/CreateOrderCommand.cs
+++ csharp-choreography-saga.OrderMicroservice/Features/CreateOrder/CreateOrderCommand.cs
@@ -1,23 +1,22 @@
 ﻿using csharp_choreography_saga.OrderMicroservice.Utils;
 using MediatR;
 
-namespace csharp_choreography_saga.OrderMicroservice.Features.CreateOrder
+namespace csharp_choreography_saga.OrderMicroservice.Features.CreateOrder;
+
+public class CreateOrderCommand : IRequest<Result<CreateOrderResponse>>
 {
-    public class CreateOrderCommand : IRequest<Result<CreateOrderResponse>>
-    {
-        public Guid UserId { get; set; }
+    public Guid UserId { get; set; }
 
-        public double GrandTotal { get; set; }
+    public double GrandTotal { get; set; }
 
-        public List<CreateOrderDetailCommand> OrderDetails { get; set; }
-    }
+    public List<CreateOrderDetailCommand> OrderDetails { get; set; }
+}
 
-    public class CreateOrderDetailCommand
-    {
-        public Guid ProductId { get; set; }
+public class CreateOrderDetailCommand
+{
+    public Guid ProductId { get; set; }
 
-        public long TotalItems { get; set; }
+    public long TotalItems { get; set; }
 
-        public double SubTotal { get; set; }
-    }
+    public double SubTotal { get; set; }
 }
