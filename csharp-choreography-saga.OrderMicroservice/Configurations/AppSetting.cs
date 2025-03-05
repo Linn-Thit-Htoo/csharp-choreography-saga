53af372fb2a3f2fe7a93de017c75b21656066da8
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-05 06:43:53 +0630

    -

--- csharp-choreography-saga.OrderMicroservice/Configurations/AppSetting.cs
+++ csharp-choreography-saga.OrderMicroservice/Configurations/AppSetting.cs
@@ -1,40 +1,39 @@
-﻿namespace csharp_choreography_saga.OrderMicroservice.Configurations
+﻿namespace csharp_choreography_saga.OrderMicroservice.Configurations;
+
+public class AppSetting
 {
-    public class AppSetting
-    {
-        public Connectionstrings ConnectionStrings { get; set; }
-        public Logging Logging { get; set; }
-        public Rabbitmq RabbitMQ { get; set; }
-    }
+    public Connectionstrings ConnectionStrings { get; set; }
+    public Logging Logging { get; set; }
+    public Rabbitmq RabbitMQ { get; set; }
+}
 
-    public class Connectionstrings
-    {
-        public string DbConnection { get; set; }
-    }
+public class Connectionstrings
+{
+    public string DbConnection { get; set; }
+}
 
-    public class Logging
-    {
-        public Loglevel LogLevel { get; set; }
-    }
+public class Logging
+{
+    public Loglevel LogLevel { get; set; }
+}
 
-    public class Loglevel
-    {
-        public string Default { get; set; }
-        public string MicrosoftAspNetCore { get; set; }
-    }
+public class Loglevel
+{
+    public string Default { get; set; }
+    public string MicrosoftAspNetCore { get; set; }
+}
 
-    public class Rabbitmq
-    {
-        public string HostName { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public Queuelist[] QueueList { get; set; }
-    }
+public class Rabbitmq
+{
+    public string HostName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public Queuelist[] QueueList { get; set; }
+}
 
-    public class Queuelist
-    {
-        public string Exchange { get; set; }
-        public string Queue { get; set; }
-        public string RoutingKey { get; set; }
-    }
+public class Queuelist
+{
+    public string Exchange { get; set; }
+    public string Queue { get; set; }
+    public string RoutingKey { get; set; }
 }
