a6961e1aa0041d33a3e08421d37badfd5b1dee72
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-14 01:04:06 +0630

    -

--- csharp-choreography-saga.StockMicroservice/Services/RabbitMQ/RabbitMQService.cs
+++ csharp-choreography-saga.StockMicroservice/Services/RabbitMQ/RabbitMQService.cs
@@ -8,77 +8,76 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
-namespace csharp_choreography_saga.StockMicroservice.Services.RabbitMQ
+namespace csharp_choreography_saga.StockMicroservice.Services.RabbitMQ;
+
+public class RabbitMQService : BackgroundService
 {
-    public class RabbitMQService : BackgroundService
+    private readonly AppSetting _appSetting;
+    private readonly ILogger<RabbitMQService> _logger;
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public RabbitMQService(ILogger<RabbitMQService> logger, IServiceScopeFactory scopeFactory, IOptions<AppSetting> options)
     {
-        private readonly AppSetting _appSetting;
-        private readonly ILogger<RabbitMQService> _logger;
-        private readonly IServiceScopeFactory _scopeFactory;
+        _logger = logger;
+        _scopeFactory = scopeFactory;
+        _appSetting = options.Value;
+    }
 
-        public RabbitMQService(ILogger<RabbitMQService> logger, IServiceScopeFactory scopeFactory, IOptions<AppSetting> options)
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        IConnection connection = CreateConnection();
+        var channel = connection.CreateModel();
+
+        foreach (var item in _appSetting.RabbitMQ.QueueList)
         {
-            _logger = logger;
-            _scopeFactory = scopeFactory;
-            _appSetting = options.Value;
-        }
+            channel.ExchangeDeclare(item.Exchange, ExchangeType.Direct, true, false, null);
+            channel.QueueDeclare(item.Queue, true, false, false);
+            channel.QueueBind(item.Queue, item.Exchange, item.RoutingKey, null);
+            channel.BasicQos(0, 1, false);
+            var consumer = new AsyncEventingBasicConsumer(channel);
 
-        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            IConnection connection = CreateConnection();
-            var channel = connection.CreateModel();
-
-            foreach (var item in _appSetting.RabbitMQ.QueueList)
+            consumer.Received += async (ch, ea) =>
             {
-                channel.ExchangeDeclare(item.Exchange, ExchangeType.Direct, true, false, null);
-                channel.QueueDeclare(item.Queue, true, false, false);
-                channel.QueueBind(item.Queue, item.Exchange, item.RoutingKey, null);
-                channel.BasicQos(0, 1, false);
-                var consumer = new AsyncEventingBasicConsumer(channel);
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
 
-                consumer.Received += async (ch, ea) =>
+                if (ea.RoutingKey.Equals("orderdirect"))
                 {
-                    var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+                    var requestModel = JsonConvert.DeserializeObject<OrderCreatedEvent>(content);
+                    var scope = _scopeFactory.CreateScope();
+                    var stockService = scope.ServiceProvider.GetRequiredService<IStockService>();
+                    var bus = scope.ServiceProvider.GetRequiredService<IBus>();
 
-                    if (ea.RoutingKey.Equals("orderdirect"))
+                    bool isStockReductionSuccessful = await stockService.ReduceStockAsync(requestModel!);
+
+                    if (!isStockReductionSuccessful)
                     {
-                        var requestModel = JsonConvert.DeserializeObject<OrderCreatedEvent>(content);
-                        var scope = _scopeFactory.CreateScope();
-                        var stockService = scope.ServiceProvider.GetRequiredService<IStockService>();
-                        var bus = scope.ServiceProvider.GetRequiredService<IBus>();
-
-                        bool isStockReductionSuccessful = await stockService.ReduceStockAsync(requestModel!);
-
-                        if (!isStockReductionSuccessful)
-                        {
-                            // publish to compensate the order
-                            CompensateOrderEvent compensateOrderEvent = new() { OrderId = requestModel!.OrderId };
-                            bus.Send("DirectExchange", "OrderQueue", "orderfaildirect",compensateOrderEvent);
-                        }
+                        // publish to compensate the order
+                        CompensateOrderEvent compensateOrderEvent = new() { OrderId = requestModel!.OrderId };
+                        bus.Send("DirectExchange", "OrderQueue", "orderfaildirect",compensateOrderEvent);
                     }
+                }
 
-                    channel.BasicAck(ea.DeliveryTag, false);
-                };
-
-                channel.BasicConsume(item.Queue, false, consumer);
-            }
-        }
-
-        private IConnection CreateConnection()
-        {
-            ConnectionFactory connectionFactory = new ConnectionFactory()
-            {
-                HostName = _appSetting.RabbitMQ.HostName,
-                UserName = _appSetting.RabbitMQ.UserName,
-                Password = _appSetting.RabbitMQ.Password,
-                VirtualHost = "/"
+                channel.BasicAck(ea.DeliveryTag, false);
             };
-            connectionFactory.AutomaticRecoveryEnabled = true;
-            connectionFactory.NetworkRecoveryInterval = TimeSpan.FromSeconds(5);
-            connectionFactory.RequestedHeartbeat = TimeSpan.FromSeconds(15);
-            connectionFactory.DispatchConsumersAsync = true;
 
-            return connectionFactory.CreateConnection();
+            channel.BasicConsume(item.Queue, false, consumer);
         }
+    }
+
+    private IConnection CreateConnection()
+    {
+        ConnectionFactory connectionFactory = new ConnectionFactory()
+        {
+            HostName = _appSetting.RabbitMQ.HostName,
+            UserName = _appSetting.RabbitMQ.UserName,
+            Password = _appSetting.RabbitMQ.Password,
+            VirtualHost = "/"
+        };
+        connectionFactory.AutomaticRecoveryEnabled = true;
+        connectionFactory.NetworkRecoveryInterval = TimeSpan.FromSeconds(5);
+        connectionFactory.RequestedHeartbeat = TimeSpan.FromSeconds(15);
+        connectionFactory.DispatchConsumersAsync = true;
+
+        return connectionFactory.CreateConnection();
     }
 }
