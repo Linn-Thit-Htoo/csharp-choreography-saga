9d86dbf4d54dec12f848d6ea1228fbdbf5c5964b
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-14 01:03:11 +0630

    -

--- csharp-choreography-saga.StockMicroservice/Models/CompensateOrderEvent.cs
+++ csharp-choreography-saga.StockMicroservice/Models/CompensateOrderEvent.cs
@@ -1,7 +1,6 @@
-﻿namespace csharp_choreography_saga.StockMicroservice.Models
+﻿namespace csharp_choreography_saga.StockMicroservice.Models;
+
+public class CompensateOrderEvent
 {
-    public class CompensateOrderEvent
-    {
-        public Guid OrderId { get; set; }
-    }
+    public Guid OrderId { get; set; }
 }
