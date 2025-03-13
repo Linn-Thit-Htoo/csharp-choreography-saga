6e5f9b284ae51351ead13ccef9f1da19480ca115
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-14 01:03:39 +0630

    -

--- csharp-choreography-saga.StockMicroservice/Persistence/Base/RepositoryBase.cs
+++ csharp-choreography-saga.StockMicroservice/Persistence/Base/RepositoryBase.cs
@@ -3,102 +3,101 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
 using System.Linq.Expressions;
 
-namespace csharp_choreography_saga.StockMicroservice.Persistence.Base
+namespace csharp_choreography_saga.StockMicroservice.Persistence.Base;
+
+public class RepositoryBase<T> : IRepositoryBase<T> where T : class
 {
-    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    internal readonly DbContext _context;
+    internal readonly DbSet<T> _dbSet;
+
+    public RepositoryBase(AppDbContext context)
     {
-        internal readonly DbContext _context;
-        internal readonly DbSet<T> _dbSet;
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
 
-        public RepositoryBase(AppDbContext context)
+    public IQueryable<T> GetAll()
+    {
+        return _dbSet.AsQueryable();
+    }
+
+    public IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression)
+    {
+        return _dbSet.Where(expression);
+    }
+
+    public IQueryable<T> Query(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null, Func<IQueryable<T>, IOrderedQueryable<T>> order = null)
+    {
+        IQueryable<T> query = _dbSet;
+
+        if (include is not null)
         {
-            _context = context;
-            _dbSet = context.Set<T>();
+            query = include(query);
+        }
+        if (expression is not null)
+        {
+            query = query.Where(expression);
+        }
+        if (order is not null)
+        {
+            query = order(query);
         }
 
-        public IQueryable<T> GetAll()
-        {
-            return _dbSet.AsQueryable();
-        }
+        return query;
+    }
 
-        public IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression)
-        {
-            return _dbSet.Where(expression);
-        }
+    public void Add(T entity)
+    {
+        _dbSet.Add(entity);
+    }
 
-        public IQueryable<T> Query(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null, Func<IQueryable<T>, IOrderedQueryable<T>> order = null)
-        {
-            IQueryable<T> query = _dbSet;
+    public void Add(IEnumerable<T> entities)
+    {
+        _dbSet.AddRange(entities);
+    }
 
-            if (include is not null)
-            {
-                query = include(query);
-            }
-            if (expression is not null)
-            {
-                query = query.Where(expression);
-            }
-            if (order is not null)
-            {
-                query = order(query);
-            }
+    public async Task AddAsync(T entity, CancellationToken cs = default)
+    {
+        await _dbSet.AddAsync(entity, cs);
+    }
 
-            return query;
-        }
+    public async Task AddAsync(IEnumerable<T> entities, CancellationToken cs = default)
+    {
+        await _dbSet.AddRangeAsync(entities, cs);
+    }
 
-        public void Add(T entity)
-        {
-            _dbSet.Add(entity);
-        }
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
 
-        public void Add(IEnumerable<T> entities)
-        {
-            _dbSet.AddRange(entities);
-        }
+    public void Update(IEnumerable<T> entities)
+    {
+        _dbSet.UpdateRange(entities);
+    }
 
-        public async Task AddAsync(T entity, CancellationToken cs = default)
-        {
-            await _dbSet.AddAsync(entity, cs);
-        }
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
 
-        public async Task AddAsync(IEnumerable<T> entities, CancellationToken cs = default)
-        {
-            await _dbSet.AddRangeAsync(entities, cs);
-        }
+    public void Delete(IEnumerable<T> entities)
+    {
+        _dbSet.RemoveRange(entities);
+    }
 
-        public void Update(T entity)
-        {
-            _dbSet.Update(entity);
-        }
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
 
-        public void Update(IEnumerable<T> entities)
-        {
-            _dbSet.UpdateRange(entities);
-        }
+    public void SaveChanges()
+    {
+        _context.SaveChanges();
+    }
 
-        public void Delete(T entity)
-        {
-            _dbSet.Remove(entity);
-        }
-
-        public void Delete(IEnumerable<T> entities)
-        {
-            _dbSet.RemoveRange(entities);
-        }
-
-        public void Dispose()
-        {
-            _context.Dispose();
-        }
-
-        public void SaveChanges()
-        {
-            _context.SaveChanges();
-        }
-
-        public async Task SaveChangesAsync(CancellationToken cs)
-        {
-            await _context.SaveChangesAsync(cs);
-        }
+    public async Task SaveChangesAsync(CancellationToken cs)
+    {
+        await _context.SaveChangesAsync(cs);
     }
 }
