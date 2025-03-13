7b9be87e96e5a8b85ceba406b6d3e954288aab0a
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-14 01:04:26 +0630

    -

--- csharp-choreography-saga.StockMicroservice/Services/Stock/StockService.cs
+++ csharp-choreography-saga.StockMicroservice/Services/Stock/StockService.cs
@@ -3,52 +3,51 @@ using csharp_choreography_saga.StockMicroservice.Models;
 using csharp_choreography_saga.StockMicroservice.Persistence.Base;
 using Microsoft.EntityFrameworkCore;
 
-namespace csharp_choreography_saga.StockMicroservice.Services.Stock
+namespace csharp_choreography_saga.StockMicroservice.Services.Stock;
+
+public class StockService : IStockService
 {
-    public class StockService : IStockService
+    private readonly IRepositoryBase<TblStock> _stockRepository;
+    private readonly ILogger<StockService> _logger;
+
+    public StockService(IRepositoryBase<TblStock> stockRepository, ILogger<StockService> logger)
     {
-        private readonly IRepositoryBase<TblStock> _stockRepository;
-        private readonly ILogger<StockService> _logger;
+        _stockRepository = stockRepository;
+        _logger = logger;
+    }
 
-        public StockService(IRepositoryBase<TblStock> stockRepository, ILogger<StockService> logger)
+    public async Task<bool> ReduceStockAsync(OrderCreatedEvent orderCreatedEvent)
+    {
+        try
         {
-            _stockRepository = stockRepository;
-            _logger = logger;
-        }
-
-        public async Task<bool> ReduceStockAsync(OrderCreatedEvent orderCreatedEvent)
-        {
-            try
+            foreach (var item in orderCreatedEvent.OrderDetails)
             {
-                foreach (var item in orderCreatedEvent.OrderDetails)
+                var stock = await _stockRepository
+                    .GetByCondition(x => x.ProductId == item.ProductId)
+                    .SingleOrDefaultAsync();
+                if (stock is null)
                 {
-                    var stock = await _stockRepository
-                        .GetByCondition(x => x.ProductId == item.ProductId)
-                        .SingleOrDefaultAsync();
-                    if (stock is null)
-                    {
-                        _logger.LogError($"Stock Not Found.");
-                        return false;
-                    }
-
-                    if (item.TotalItems > stock.Stock)
-                    {
-                        _logger.LogError($"Insufficient Stock.");
-                        return false;
-                    }
-
-                    var resultStock = stock.Stock - item.TotalItems;
-                    _stockRepository.Update(stock);
-                    await _stockRepository.SaveChangesAsync();
+                    _logger.LogError($"Stock Not Found.");
+                    return false;
                 }
 
-                return true;
+                if (item.TotalItems > stock.Stock)
+                {
+                    _logger.LogError($"Insufficient Stock.");
+                    return false;
+                }
+
+                var resultStock = stock.Stock - item.TotalItems;
+                _stockRepository.Update(stock);
+                await _stockRepository.SaveChangesAsync();
             }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Stock Reduction Error: {ex.ToString()}");
-                return false;
-            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Stock Reduction Error: {ex.ToString()}");
+            return false;
         }
     }
 }
