eef7b897d23d5374567ccd4a71acc5b2d7ea2674
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-14 01:01:42 +0630

    -

--- csharp-choreography-saga.OrderMicroservice/Services/RabbitMQ/RabbitBus.cs
+++ csharp-choreography-saga.OrderMicroservice/Services/RabbitMQ/RabbitBus.cs
@@ -3,77 +3,76 @@ using Newtonsoft.Json;
 using RabbitMQ.Client;
 using System.Text;
 
-namespace csharp_choreography_saga.OrderMicroservice.Services.RabbitMQ
+namespace csharp_choreography_saga.OrderMicroservice.Services.RabbitMQ;
+
+public interface IBus
 {
-    public interface IBus
+    void Send<T>(string exchange, string queue, string routingKey, T message);
+}
+
+public class RabbitBus : IBus
+{
+    internal readonly AppSetting _appSetting;
+
+    public RabbitBus(IConfiguration configuration)
     {
-        void Send<T>(string exchange, string queue, string routingKey, T message);
+        _appSetting = configuration.Get<AppSetting>()!;
     }
 
-    public class RabbitBus : IBus
+    public void Send<T>(string exchange, string queue, string routingKey, T message)
     {
-        internal readonly AppSetting _appSetting;
+        IConnection connection = CreateConnection();
+        var channel = connection.CreateModel();
 
-        public RabbitBus(IConfiguration configuration)
+        channel.ExchangeDeclare(
+            exchange: exchange,
+            type: ExchangeType.Direct,
+            durable: true,
+            autoDelete: false,
+            arguments: null
+        );
+
+        channel.QueueDeclare(
+            queue: queue,
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        channel.QueueBind(
+            queue: queue,
+            exchange: exchange,
+            routingKey: routingKey,
+            arguments: null
+        );
+
+        var properties = channel.CreateBasicProperties();
+        properties.Persistent = true;
+
+        var messageBody = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+        channel.BasicPublish(
+            exchange: exchange,
+            routingKey: routingKey,
+            basicProperties: properties,
+            body: messageBody
+        );
+    }
+
+    private IConnection CreateConnection()
+    {
+        ConnectionFactory connectionFactory = new ConnectionFactory()
         {
-            _appSetting = configuration.Get<AppSetting>()!;
-        }
+            HostName = _appSetting.RabbitMQ.HostName,
+            UserName = _appSetting.RabbitMQ.UserName,
+            Password = _appSetting.RabbitMQ.Password,
+            VirtualHost = "/"
+        };
+        connectionFactory.AutomaticRecoveryEnabled = true;
+        connectionFactory.NetworkRecoveryInterval = TimeSpan.FromSeconds(5);
+        connectionFactory.RequestedHeartbeat = TimeSpan.FromSeconds(15);
+        connectionFactory.DispatchConsumersAsync = true;
 
-        public void Send<T>(string exchange, string queue, string routingKey, T message)
-        {
-            IConnection connection = CreateConnection();
-            var channel = connection.CreateModel();
-
-            channel.ExchangeDeclare(
-                exchange: exchange,
-                type: ExchangeType.Direct,
-                durable: true,
-                autoDelete: false,
-                arguments: null
-            );
-
-            channel.QueueDeclare(
-                queue: queue,
-                durable: true,
-                exclusive: false,
-                autoDelete: false,
-                arguments: null
-            );
-
-            channel.QueueBind(
-                queue: queue,
-                exchange: exchange,
-                routingKey: routingKey,
-                arguments: null
-            );
-
-            var properties = channel.CreateBasicProperties();
-            properties.Persistent = true;
-
-            var messageBody = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
-            channel.BasicPublish(
-                exchange: exchange,
-                routingKey: routingKey,
-                basicProperties: properties,
-                body: messageBody
-            );
-        }
-
-        private IConnection CreateConnection()
-        {
-            ConnectionFactory connectionFactory = new ConnectionFactory()
-            {
-                HostName = _appSetting.RabbitMQ.HostName,
-                UserName = _appSetting.RabbitMQ.UserName,
-                Password = _appSetting.RabbitMQ.Password,
-                VirtualHost = "/"
-            };
-            connectionFactory.AutomaticRecoveryEnabled = true;
-            connectionFactory.NetworkRecoveryInterval = TimeSpan.FromSeconds(5);
-            connectionFactory.RequestedHeartbeat = TimeSpan.FromSeconds(15);
-            connectionFactory.DispatchConsumersAsync = true;
-
-            return connectionFactory.CreateConnection();
-        }
+        return connectionFactory.CreateConnection();
     }
 }
