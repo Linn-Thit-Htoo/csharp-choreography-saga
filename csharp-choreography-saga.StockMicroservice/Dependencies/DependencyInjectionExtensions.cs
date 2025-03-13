dc476ab936cf22e0a541fc67f881308edcbece05
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-14 01:02:46 +0630

    -

--- csharp-choreography-saga.StockMicroservice/Dependencies/DependencyInjectionExtensions.cs
+++ csharp-choreography-saga.StockMicroservice/Dependencies/DependencyInjectionExtensions.cs
@@ -7,54 +7,53 @@ using csharp_choreography_saga.StockMicroservice.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace csharp_choreography_saga.StockMicroservice.Dependencies
+namespace csharp_choreography_saga.StockMicroservice.Dependencies;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencie(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencie(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
+        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
 
-            builder.Services.Configure<AppSetting>(builder.Configuration);
+        builder.Services.Configure<AppSetting>(builder.Configuration);
 
-            builder
-                .Services.AddControllers()
-                .ConfigureApiBehaviorOptions(opt =>
-                {
-                    opt.InvalidModelStateResponseFactory = context =>
-                    {
-                        var errors = context
-                            .ModelState.Where(ms => ms.Value!.Errors.Any())
-                            .SelectMany(ms => ms.Value!.Errors.Select(e => e.ErrorMessage))
-                            .ToList();
-
-                        var errorMessage = string.Join(" ", errors);
-                        var result = Result<object>.Fail(
-                            errorMessage
-                        );
-
-                        return new OkObjectResult(result);
-                    };
-                })
-                .AddJsonOptions(opt =>
-                {
-                    opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-                });
-
-            builder.Services.AddDbContext<AppDbContext>((serviceProvider, opt) =>
+        builder
+            .Services.AddControllers()
+            .ConfigureApiBehaviorOptions(opt =>
             {
-                opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection"));
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                opt.InvalidModelStateResponseFactory = context =>
+                {
+                    var errors = context
+                        .ModelState.Where(ms => ms.Value!.Errors.Any())
+                        .SelectMany(ms => ms.Value!.Errors.Select(e => e.ErrorMessage))
+                        .ToList();
+
+                    var errorMessage = string.Join(" ", errors);
+                    var result = Result<object>.Fail(
+                        errorMessage
+                    );
+
+                    return new OkObjectResult(result);
+                };
+            })
+            .AddJsonOptions(opt =>
+            {
+                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
             });
 
-            builder.Services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
-            builder.Services.AddScoped<IStockService, StockService>();
-            builder.Services.AddHostedService<RabbitMQService>();
-            builder.Services.AddSingleton<IBus, RabbitBus>();
+        builder.Services.AddDbContext<AppDbContext>((serviceProvider, opt) =>
+        {
+            opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection"));
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        });
 
-            return services;
-        }
+        builder.Services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+        builder.Services.AddScoped<IStockService, StockService>();
+        builder.Services.AddHostedService<RabbitMQService>();
+        builder.Services.AddSingleton<IBus, RabbitBus>();
+
+        return services;
     }
 }
