caa0b7a655b0043e662b2cd695ac08d307e478a2
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-14 01:01:24 +0630

    -

--- csharp-choreography-saga.OrderMicroservice/Services/Order/IOrderService.cs
+++ csharp-choreography-saga.OrderMicroservice/Services/Order/IOrderService.cs
@@ -1,10 +1,9 @@
 ﻿using csharp_choreography_saga.OrderMicroservice.Models;
 
-namespace csharp_choreography_saga.OrderMicroservice.Services.Order
+namespace csharp_choreography_saga.OrderMicroservice.Services.Order;
+
+public interface IOrderService
 {
-    public interface IOrderService
-    {
-        Task CompensateOrderAsync(CompensateOrderEvent compensateOrderEvent);
-        Task CompensateOrderAsyncV1(CompensateOrderEvent compensateOrderEvent);
-    }
+    Task CompensateOrderAsync(CompensateOrderEvent compensateOrderEvent);
+    Task CompensateOrderAsyncV1(CompensateOrderEvent compensateOrderEvent);
 }
